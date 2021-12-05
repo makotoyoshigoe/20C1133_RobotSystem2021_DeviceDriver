7bdadc72420588040f25e315dff5425b3ee49b9f
makotoyoshigoe <myoshigo0127@gmail.com>
2021-12-05 15:18:50 +0000

    Update file

--- calc.py
+++ calc.py
@@ -47,27 +47,18 @@ def inverse_bit(arr):
             arr[i] = 0
     return arr
 
-def write_to_file(path, value):
-    with open(path, mode='w') as f:
-        f.write(value)
-
 def main():
     formula = ''
+    path = 'log.txt'
     while formula != 'q':
         formula = input('input formula:')
         try:
             ans = eval(formula)
-            if ans < 0:
-                binary = convert_binary(abs(ans))
-                binary = inverse_bit(binary)
-
-            else:
-                binary = convert_binary(ans)
+            binary = inverse_bit(convert_binary(abs(ans))) if ans < 0 else convert_binary(ans)
             print(f'ans={ans}, {binary}')
-            with open('log.txt', mode='w') as f:
+            with open(path, mode='w') as f:
                 for i in range(len(binary)):
                     f.write(str(binary[i]))
-           # write_to_file('calculator/log.txt', 1)
         except:
             print('this formula cannot be calculated.')
 
