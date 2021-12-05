8f0fa2072f5eec094b353d80acd05ec234c6d9d8
makotoyoshigoe <myoshigo0127@gmail.com>
2021-12-05 15:07:52 +0000

    Update file

--- calc.py
+++ calc.py
@@ -10,7 +10,7 @@ def convert_binary_int(dec):
         if quo < 1:
             binary.append(quo)
             break
-    print(len(binary))
+    #print(len(binary))
     while len(binary) < 12:
         binary.append(0)
     binary.reverse()
@@ -23,26 +23,56 @@ def convert_binary(dec):
     p_float = dec - p_int
     f_bin = []
     i_bin = convert_binary_int(p_int)
-    i_bin.append(1)
+    i_bin.append(2)
     cnt = 0
     quo = p_float
-    while cnt < 4:
-        cnt += 1
+    for i in range(4):
         quo *= 2
         f_bin.append(int(quo))
         quo -= 1 if quo >= 1 else 0
     binary = i_bin + f_bin
     return binary
 
-def main():
-    fomula = ''
-    while True:
-        fomula = input('input formula:')
-        if fomula == 'q':
+def inverse_bit(arr):
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            arr[i] = 1
+        elif arr[i] == 1:
+            arr[i] = 0
+    for i in range(-1, -len(arr), -1):
+        if arr[i] == 0:
+            arr[i] = 1
             break
-        ans = eval(fomula)
-        binary = convert_binary(ans)
-        print(f'ans={ans}, binary={binary}')
+        elif arr[i] == 1:
+            arr[i] = 0
+    return arr
+
+def write_to_file(path, value):
+    with open(path, mode='w') as f:
+        f.write(value)
+
+def main():
+    formula = ''
+    while formula != 'q':
+        formula = input('input formula:')
+        try:
+            ans = eval(formula)
+            if ans < 0:
+                binary = convert_binary(abs(ans))
+                binary = inverse_bit(binary)
+
+            else:
+                binary = convert_binary(ans)
+            print(f'ans={ans}, {binary}')
+            with open('log.txt', mode='w') as f:
+                for i in range(len(binary)):
+                    f.write(str(binary[i]))
+           # write_to_file('calculator/log.txt', 1)
+        except:
+            print('this formula cannot be calculated.')
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except:
+        print('error')
